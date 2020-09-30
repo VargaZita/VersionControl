e8c4460f28edc41e8de2114e6c93c827af68967e
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-09-30 21:57:02 +0200

    feladat7

--- gyak04_NKJBXO/gyak04_NKJBXO/Form1.cs
+++ gyak04_NKJBXO/gyak04_NKJBXO/Form1.cs
@@ -18,11 +18,69 @@ namespace gyak04_NKJBXO
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
         public Form1()
         {
             InitializeComponent();
 
             LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            //throw new NotImplementedException();
+
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable(); 
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            //throw new NotImplementedException();
+
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+
+            }
         }
 
         private void LoadData()
