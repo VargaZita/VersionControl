754fddb8af448e05f71925409200e8b0d60a7f32
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-10-07 13:25:30 +0200

    feladat4

--- gyak05_NKJBXO/gyak05_NKJBXO/Form1.cs
+++ gyak05_NKJBXO/gyak05_NKJBXO/Form1.cs
@@ -12,9 +12,16 @@ namespace gyak05_NKJBXO
 {
     public partial class Form1 : Form
     {
+
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
