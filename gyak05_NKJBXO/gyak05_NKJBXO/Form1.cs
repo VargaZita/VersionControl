4caefcb17fe73c68312c37dcb4713d81b47c7949
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-10-07 13:32:16 +0200

    feladat5

--- gyak05_NKJBXO/gyak05_NKJBXO/Form1.cs
+++ gyak05_NKJBXO/gyak05_NKJBXO/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak05_NKJBXO.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,12 +17,27 @@ namespace gyak05_NKJBXO
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
 
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            //throw new NotImplementedException();
+
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
