835015154aa27d61d3c71ac4da6549235ed7848c
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-10-14 17:57:19 +0200

    feladat5

--- gyak06_NKJBXO/gyak06_NKJBXO/Form1.cs
+++ gyak06_NKJBXO/gyak06_NKJBXO/Form1.cs
@@ -9,26 +9,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace gyak06_NKJBXO
 {
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
 
-            WebSzolgaltatas();
-
-            BindingList<RateData> Rates = new BindingList<RateData>();
-
+                       
             dataGridView1.DataSource = Rates;
+
+            XMLFeldolgozas(WebSzolgaltatas());
         }
 
-        private void WebSzolgaltatas()
+        private void XMLFeldolgozas(string result)
         {
-            //throw new NotImplementedException();
 
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+
+
+
+        }
+
+        private string WebSzolgaltatas()
+        {
+            
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
@@ -41,6 +69,9 @@ namespace gyak06_NKJBXO
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+
+
+            return result;
         }
     }
 }
