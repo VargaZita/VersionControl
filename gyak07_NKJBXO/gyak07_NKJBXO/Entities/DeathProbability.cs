3ffede4fa34bddd38797ab272c5e49cb8fb61635
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-10-21 14:45:42 +0200

    feladat3

---
+++ gyak07_NKJBXO/gyak07_NKJBXO/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak07_NKJBXO.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double DProbability { get; set; }
+    }
+}
