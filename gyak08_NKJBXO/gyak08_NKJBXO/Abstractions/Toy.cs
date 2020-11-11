80a7439a024b3887d821bdbd26a6ea01fcc85843
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-11-11 16:10:43 +0100

    feladat5

--- gyak08_NKJBXO/gyak08_NKJBXO/Abstractions/Toy.cs
+++ gyak08_NKJBXO/gyak08_NKJBXO/Abstractions/Toy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,24 @@ namespace gyak08_NKJBXO.Abstractions
 {
     public abstract class Toy: Label
     {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
 
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics g);
+
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
     }
 }
