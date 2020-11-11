914114cc4c308f3ea166b90456a71c104249d127
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-11-11 16:27:28 +0100

    feladat8

---
+++ gyak08_NKJBXO/gyak08_NKJBXO/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak08_NKJBXO.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+
+    }
+}
