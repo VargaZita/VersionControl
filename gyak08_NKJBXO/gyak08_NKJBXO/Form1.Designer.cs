80430ed6b21b489e82f0dd7bcee8b6191c272003
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-11-11 19:48:17 +0100

    feladat12

--- gyak08_NKJBXO/gyak08_NKJBXO/Form1.Designer.cs
+++ gyak08_NKJBXO/gyak08_NKJBXO/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.btnSelectCar = new System.Windows.Forms.Button();
             this.btnSelectBall = new System.Windows.Forms.Button();
             this.lblNext = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,6 +44,7 @@
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Controls.Add(this.btnColor);
             this.mainPanel.Controls.Add(this.lblNext);
             this.mainPanel.Controls.Add(this.btnSelectBall);
             this.mainPanel.Controls.Add(this.btnSelectCar);
@@ -95,6 +97,16 @@
             this.lblNext.TabIndex = 1;
             this.lblNext.Text = "Coming next:";
             // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Blue;
+            this.btnColor.Location = new System.Drawing.Point(105, 66);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(88, 23);
+            this.btnColor.TabIndex = 2;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -117,6 +129,7 @@
         private System.Windows.Forms.Label lblNext;
         private System.Windows.Forms.Button btnSelectBall;
         private System.Windows.Forms.Button btnSelectCar;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
