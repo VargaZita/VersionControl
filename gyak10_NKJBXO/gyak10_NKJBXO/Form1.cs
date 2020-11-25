5a9475b1b75094f977e04b4bbdfaf103961aed9b
VargaZita <zita.varga@stud.uni-corvinus.hu>
2020-11-25 16:38:47 +0100

    feladat3

--- gyak10_NKJBXO/gyak10_NKJBXO/Form1.cs
+++ gyak10_NKJBXO/gyak10_NKJBXO/Form1.cs
@@ -22,6 +22,9 @@ namespace gyak10_NKJBXO
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            //gc.AddPlayer();
+            //gc.Start(true);
         }
     }
 }
